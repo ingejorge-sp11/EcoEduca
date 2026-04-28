bba94a8eb7e5eaee87c8b016858c101b7316f1c6
Jorge Olivares <144082526+ingejorge-sp11@users.noreply.github.com>
2026-04-27 22:24:10 -0600

    Rename input field from 'codigo_estudiante' to 'codigo'

--- EcoEducaAPP/src/App.jsx
+++ EcoEducaAPP/src/App.jsx
@@ -269,7 +269,7 @@ const RegisterForm = ({ onSwitchToLogin }) => {
             </div>
             <div>
                 <label className="block text-sm font-medium text-gray-700">Código de Estudiante</label>
-                <input type="text" name="codigo_estudiante" onChange={handleChange} placeholder="Ej. 217XXXXXX" required className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"/>
+                <input type="text" name="codigo" onChange={handleChange} placeholder="Ej. 217XXXXXX" required className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"/>
             </div>
             <div>
                 <label className="block text-sm font-medium text-gray-700">Contraseña</label>
